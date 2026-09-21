98aa018c40a3c3b58e9ca1912351369a8ad971bc
angadsingh-stack <saytam135@gmail.com>
2026-09-21 21:18:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ async function api(url, options = {}) {
 }
 
 async function loadCourses() {
-  state.courses = await api('/api/courses');
+  state.courses = await api('/api/courses''https://e-earningwebsite.onrender.com/' );
   if (state.token) {
     try { state.progress = await api('/api/progress'); }
     catch { logout(); }
